983efbcf40f4fb8d93efc4a423fa47cf7c1b457c
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-10-31 17:26:17 +0100

    adding nested routes in app.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 import AppNav from "./AppNav";
 import Logo from "./Logo";
 
@@ -9,7 +11,8 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of Cities</p>
+      <Outlet />
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
