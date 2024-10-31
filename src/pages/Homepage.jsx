f721dce711c78b3c476a834b8b27bc2a755e64ef
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-10-31 17:01:34 +0100

    preparing sidebar

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 
-import styles from "./Homepage.module.css";
 import PageNav from "../components/PageNav";
+
+import styles from "./Homepage.module.css";
 
 export default function Homepage() {
   return (
