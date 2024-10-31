c92403b234a9dce959abf9b8da0417989dda5b0f
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-10-31 14:20:45 +0100

    switching from Link to NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
-import Homepage from "./pages/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="home" element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
