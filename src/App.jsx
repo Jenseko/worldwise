c387a2c4931944ffc6b9ea500063766c55c717fd
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-10-31 14:14:50 +0100

    implementing navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="home" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Home</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default Homepage;
