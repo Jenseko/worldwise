c5080014d5884cc8f4791d1cedb56b35af0148b2
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-11-06 11:14:04 +0100

    complete deletecity function

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -10,13 +10,13 @@ const formatDate = date =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
 
   const { cityName, emoji, date, id, position } = city;
 
   function handleClick(e) {
     e.preventDefault();
-    console.log("TEST");
+    deleteCity(id);
   }
 
   return (
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -49,10 +49,24 @@ function CitiesProvider({ children }) {
         }
       });
       const data = await res.json();
-
       setCities(cities => [...cities, data]);
     } catch (err) {
-      alert("There was an error loading data...");
+      alert("There was an error creating city!");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE"
+      });
+
+      setCities(cities => cities.filter(city => city.id !== id));
+    } catch (err) {
+      alert("There was an error deleting city!");
     } finally {
       setIsLoading(false);
     }
@@ -65,7 +79,8 @@ function CitiesProvider({ children }) {
         isLoading,
         currentCity,
         getCity,
-        createCity
+        createCity,
+        deleteCity
       }}>
       {children}
     </CitiesContext.Provider>
